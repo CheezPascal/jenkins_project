f7c3394b331561cccbab8128071ada5ac5188739
Chisom Pascal Ezeaniekwe <rascal.chisom@gmail.com>
2024-09-13 12:24:12 -0400

    Updated Python code

--- chisom.py
+++ chisom.py
@@ -1,6 +1,6 @@
 # This code is a simple one that calculates the length of the passed argument, Version 1 of app 
 # Name of this app is --Jenkins-comment (python-app in Jenkinsfile)----
-#This comment was added to initiate the third build 
+#This comment was added to initiate the second build 
 
 comment1 = "Jenkins course was an interesting one..."
 x = len(comment1) 
